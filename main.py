49c20de741ebee7f1b30893ab47d39edacda904a
mwtrager <mtrager20@gmail.com>
2018-01-17 20:38:02 -0500

    get loading new palettes down and this is a building point for a loop

--- main.py
+++ main.py
@@ -3,17 +3,64 @@ from selenium import webdriver
 import time
 import os
 
-# NOTE not necessary?
-print('setting up chrome options...')
+print('making webdriver...')
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
-chrome_options.binary_location = '/usr/local/bin/chromedriver'
-
-print('making webdriver...')
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(chrome_options=chrome_options)
 
 print('getting page...')
 driver.get('http://colorhunt.co')
-print(driver.page_source)
+before_source = driver.page_source
+
+print('try scrolling...')
+driver.execute_script('window.scrollTo(0,document.querySelector("#container").scrollHeight)')
+time.sleep(3) # NOTE without this sleep, the source doesn't get updated quick enough
+after_source = driver.page_source
+
+print('get the 1 palettes...')
+soup = ws.soupify(after_source)
+palettes = ws.get_palettes(soup)
+print(len(palettes[:-1]))
+for palette in palettes[:-1]:
+    print(palette)
+
+print('get the 2 palettes...')
+driver.execute_script('window.scrollTo(0,document.querySelector("#container").scrollHeight)')
+time.sleep(3) # NOTE without this sleep, the source doesn't get updated quick enough
+soup = ws.soupify(driver.page_source)
+palettes = ws.get_palettes(soup)
+print(len(palettes[:-1]))
+for palette in palettes[:-1]:
+    print(palette)
+
+print('get the 3 palettes...')
+driver.execute_script('window.scrollTo(0,document.querySelector("#container").scrollHeight)')
+time.sleep(3) # NOTE without this sleep, the source doesn't get updated quick enough
+soup = ws.soupify(driver.page_source)
+palettes = ws.get_palettes(soup)
+print(len(palettes[:-1]))
+for palette in palettes[:-1]:
+    print(palette)
+
+print('get the 4 palettes...')
+driver.execute_script('window.scrollTo(0,document.querySelector("#container").scrollHeight)')
+time.sleep(3) # NOTE without this sleep, the source doesn't get updated quick enough
+soup = ws.soupify(driver.page_source)
+palettes = ws.get_palettes(soup)
+print(len(palettes[:-1]))
+for palette in palettes[:-1]:
+    print(palette)
+
+print('get the 5 palettes...')
+driver.execute_script('window.scrollTo(0,document.querySelector("#container").scrollHeight)')
+time.sleep(3) # NOTE without this sleep, the source doesn't get updated quick enough
+soup = ws.soupify(driver.page_source)
+palettes = ws.get_palettes(soup)
+print(len(palettes[:-1]))
+for palette in palettes[:-1]:
+    print(palette)
+
+
+
 
 driver.close()
--- webscraper.py
+++ webscraper.py
@@ -4,12 +4,19 @@ import re
 from bs4 import BeautifulSoup
 
 # soupify me captain!
-def soupify(webpage):
-    r = get(webpage)
-    html = r.text
+def soupify(html):
+    # r = get(webpage)
+    # html = r.text
     soup =  BeautifulSoup(html, 'html.parser')
     return soup
 
-def find_colors(soup):
-    # taking in a soup... get some colors!
-    return 0
+# list of lists (each inner list is a palette)
+def get_palettes(soup):
+    palettes = []
+    palette_divs = soup('div', 'palette')
+    for div in palette_divs:
+        palette = []
+        for span in div('span'):
+            palette.append(span.get_text())
+        palettes.append(palette)
+    return palettes
