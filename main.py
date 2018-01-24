4dfa55b4f6cde8c5f46b2e565b52f5225184252a
mwtrager <mtrager20@gmail.com>
2018-01-24 08:22:56 -0500

    remove os, get output to csv correct

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import webscraper as ws
 from selenium import webdriver
 import time
-import os
 
 print('making webdriver...')
 chrome_options = webdriver.ChromeOptions()
@@ -9,7 +8,7 @@ chrome_options.add_argument('--headless')
 driver = webdriver.Chrome(chrome_options=chrome_options)
 
 print('getting page...')
-driver.get('http://colorhunt.co')
+driver.get('http://colorhunt.co/popular')
 before_source = driver.page_source
 
 print('try scrolling...')
@@ -17,50 +16,30 @@ driver.execute_script('window.scrollTo(0,document.querySelector("#container").sc
 time.sleep(3) # NOTE without this sleep, the source doesn't get updated quick enough
 after_source = driver.page_source
 
-print('get the 1 palettes...')
-soup = ws.soupify(after_source)
-palettes = ws.get_palettes(soup)
-print(len(palettes[:-1]))
-for palette in palettes[:-1]:
-    print(palette)
+target = 10
+count = 0
+iteration = 0
+while count < target:
+    driver.execute_script('window.scrollTo(0,document.querySelector("#container").scrollHeight)')
+    time.sleep(3) # NOTE without this sleep, the source doesn't get updated quick enough
+    soup = ws.soupify(driver.page_source)
+    palettes = ws.get_palettes(soup) # TODO rewrite with selenium
+    count = len(palettes[:-1])
+    print('current count is:', len(palettes[:-1]), 'on iteration', iteration)
+    iteration = iteration + 1
 
-print('get the 2 palettes...')
-driver.execute_script('window.scrollTo(0,document.querySelector("#container").scrollHeight)')
-time.sleep(3) # NOTE without this sleep, the source doesn't get updated quick enough
+
+print('count is', count)
+print('grabbing all of them...')
 soup = ws.soupify(driver.page_source)
+driver.close() # NOTE done with driver now, close it
 palettes = ws.get_palettes(soup)
-print(len(palettes[:-1]))
-for palette in palettes[:-1]:
-    print(palette)
 
-print('get the 3 palettes...')
-driver.execute_script('window.scrollTo(0,document.querySelector("#container").scrollHeight)')
-time.sleep(3) # NOTE without this sleep, the source doesn't get updated quick enough
-soup = ws.soupify(driver.page_source)
-palettes = ws.get_palettes(soup)
-print(len(palettes[:-1]))
-for palette in palettes[:-1]:
-    print(palette)
-
-print('get the 4 palettes...')
-driver.execute_script('window.scrollTo(0,document.querySelector("#container").scrollHeight)')
-time.sleep(3) # NOTE without this sleep, the source doesn't get updated quick enough
-soup = ws.soupify(driver.page_source)
-palettes = ws.get_palettes(soup)
-print(len(palettes[:-1]))
-for palette in palettes[:-1]:
-    print(palette)
-
-print('get the 5 palettes...')
-driver.execute_script('window.scrollTo(0,document.querySelector("#container").scrollHeight)')
-time.sleep(3) # NOTE without this sleep, the source doesn't get updated quick enough
-soup = ws.soupify(driver.page_source)
-palettes = ws.get_palettes(soup)
-print(len(palettes[:-1]))
-for palette in palettes[:-1]:
-    print(palette)
-
-
-
-
-driver.close()
+with open("palettes.csv", "w") as file:
+    for palette in palettes[:-1]:
+        for color in reversed(palette): # NOTE the colors in the palette come in reversed so change it back
+            if color == palette[0]:
+                file.write('"%s"' % color)
+            else:
+                file.write('"%s",' % color)
+        file.write('\n')
